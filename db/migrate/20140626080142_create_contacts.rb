f9d2d9db9660a207d74595ad77c4346c1c8516ef
Michał Matyas <github@higher.lv>
2014-07-08 13:04:57 +0200

    Add unique index to contacts email

--- db/migrate/20140626080142_create_contacts.rb
+++ db/migrate/20140626080142_create_contacts.rb
@@ -2,8 +2,8 @@ class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
       t.string :email, unique: true
-
       t.timestamps
     end
+    add_index :contacts, :email, :unique => true
   end
 end
