509fad29d4db6a5468830826305acaaf5e815e3e
rabsztok <rabsztok@gmail.com>
2014-07-09 09:18:05 +0200

    removed .rb in requires

--- lib/sledgehammer.rb
+++ lib/sledgehammer.rb
@@ -1,11 +1,11 @@
 require 'sidekiq'
 require 'typhoeus'
 require 'sledgehammer/version'
-require 'sledgehammer/models/contact.rb'
-require 'sledgehammer/models/page.rb'
-require 'sledgehammer/models/page_contact.rb'
-require 'sledgehammer/models/website.rb'
-require 'sledgehammer/workers/crawl_worker.rb'
+require 'sledgehammer/models/contact'
+require 'sledgehammer/models/page'
+require 'sledgehammer/models/page_contact'
+require 'sledgehammer/models/website'
+require 'sledgehammer/workers/crawl_worker'
 
 module Sledgehammer
 end
